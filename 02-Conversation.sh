06a8c026769825b3ac317cfaa78efd646d9a86a3
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-23 17:53:38 +0530

    create new file

---
+++ 02-Conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi suresh! how are you."
+echo "suresh :: Hi ramesh! am fine what about you."
+echo "Ramesh :: yes suresh! am good let have some tea"
+echo "Suresh :: okay Ramesh! let go"
