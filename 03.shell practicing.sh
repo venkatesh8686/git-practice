4c80c650fafa6a9d86a927f74467e43d710db306
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-25 22:41:08 +0530

    added new file

---
+++ 03.shell practicing.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo " Please enter your name::"
+
+read -s USERNAME
+echo "username enetred is:$USERNAME"
+echo " please enter password::"
+read -s PASSWORD
+echo " password is :$PASSWORD"
