03315ee2ad49435c1d3be85bcd253f0256ef4af0
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-27 12:23:34 +0530

    new line

--- 06.spclvar.sh
+++ 06.spclvar.sh
@@ -3,3 +3,7 @@
 echo "type any char are spcl char are number you want: $*"
 echo "We can see how many var in line: $#"
 echo "Who which dir in present: $0"
+echo "we can the path: $PWD"
+echo "show home dir of current user: $HOME"
+echo "show pid of this script: $$"
+echo "show of last background : $!"
