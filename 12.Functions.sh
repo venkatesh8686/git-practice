5966c0e2164b46ae87d1c28d4dbe9816f2235f26
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-29 20:20:32 +0530

    this practing for installing

--- 12.Functions.sh
+++ 12.Functions.sh
@@ -13,36 +13,35 @@ then
 fi
 
 VALIDATION(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 mysql is not installed.. check the cmd"
+    else
+        echo "$2 mysql is installing"
+    fi
 
-    echo "exit code is : $1"
 
 }
 
-dnf lists installed git 
+dnf list installed git 
 
-VALIDATION $?
+VALIDATION $? "LIsiting git"
 
-# if [ $? -ne 0 ]
-# then 
-#     echo "git is not installing, Please wait git is ready to install"
-#     dnf install git -y
-# else
-#     echo "git is allready installed"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "git is not installing, Please wait git is ready to install"
+    dnf install git -y
+else
+    echo "git is allready installed"
+fi
 
-# dnf list installed mysql
+dnf list installed mysql
 
-# if [ $? -ne 0 ]
-# then
-#     echo "mysql is ready to install is in progress"
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "mysql is not installed.. check the cmd"
-#         exit 1
-#     else
-#         echo "mysql is installing"
-#     fi
-# else
-#     echo "mysql is already installed"
-# fi
+if [ $? -ne 0 ]
+then
+    echo "mysql is ready to install is in progress"
+    dnf install mysql -y
+    VALIDATION $? "installing mysql"
+else
+    echo "mysql is already installed"
+fi
