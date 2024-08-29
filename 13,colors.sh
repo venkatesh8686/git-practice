ec0d8ee6867ab5e0b8a8d3170dc76beca49e837e
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-08-29 20:39:47 +0530

    this practing for installing

---
+++ 13,colors.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#echo "user id is : $USERID"
+
+if [ $USERID -ne 0 ]
+then
+  echo "please try with superuser"
+  exit 1
+
+fi
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 mysql is not $R installed.. check the cmd":$N
+    else
+        echo -e "$2 mysql $G is installing"
+    fi
+
+
+}
+
+dnf list installed git 
+
+VALIDATION $? "LIsiting git"
+
+if [ $? -ne 0 ]
+then 
+    echo "git is not installing, Please wait git is ready to install"
+    dnf install git -y
+else
+    echo "git is allready installed"
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "mysql is ready to install is in progress"
+    dnf install mysql -y
+    VALIDATION $? "installed is done mysql"
+    systemctl status mysql
+else
+    echo "mysql is already installed"
+fi
