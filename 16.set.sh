609afab0f2c162fa4ff6fac0d96b67ce4d1ceebf
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-09-01 17:06:08 +0530

    set practice

--- 16.set.sh
+++ 16.set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-set -ex 
+set -ex
+
+failuer(){
+    echo "Failed at: $1:$2"
+
+}
+trap 'failuer "${LINENO}" "$BASH_COMMAND" '
 echo "hi venki keep goign"
 echhhh "hello world"
 echo "hii venki"
