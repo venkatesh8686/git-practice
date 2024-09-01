ecfea9c2922dad2e9b651ea258733015ea4126f8
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-09-01 17:13:37 +0530

    set practice

--- 16.set.sh
+++ 16.set.sh
@@ -4,10 +4,9 @@ set -e
 
 failure(){
     echo "Failed at: $1:$2"
-
 }
 
-trap 'failure "${LINENO}" "$BASH_COMMAND"'
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR
 
 echo "hi venki keep goign"
 echhhh "hello world"
